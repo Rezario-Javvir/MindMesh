ce5b6499b25944703ae5b3b101880ff77d5cf23d
rendixk <ferendfio@gmail.com>
2025-10-12 09:03:17 +0700

    Remove: unused file

--- be_mindmesh/src/modules/user/auth/auth.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { controller_register, controller_login } from "./auth.controller.ts"
-
-const router = Router()
-
-router.post("/register", controller_register)
-router.post("/login", controller_login)
-
-export default router
