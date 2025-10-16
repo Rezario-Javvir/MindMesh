bcbd3290ee603a97e038132cf3cc7c8af5a010f5
Rezario-Javvir <hendrarizqikurniawa@gmail.com>
2025-10-16 09:46:06 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home"
 import Account from "./pages/Account"
 import Profile from "./pages/Profile"
 import AddArticle from "./pages/AddArticle"
+import Login from "./pages/Login"
+import TestPage from "./pages/TestPage"
+
 
 function App() {
 
@@ -14,6 +17,8 @@ function App() {
           <Route path="/Profile" element={<Profile/>}></Route>
           <Route path="/Account" element={<Account/>}></Route>
           <Route path="/AddArticle" element={<AddArticle/>}></Route>
+          <Route path="/Login" element={<Login/>}></Route>
+          <Route path="/TestPage" element={<TestPage/>}></Route>
         </Routes>
       </Router>
     </div>
