e08dc29b7db33a4e9714033e3f5a8bc2866df067
rendixk <ferendfio@gmail.com>
2025-10-19 13:21:12 +0700

    configure upload image function

--- be_mindmesh/src/app.ts
+++ be_mindmesh/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import cors from "cors"
+import path from 'path'
 import { errorHandler } from './middleware/error.middleware.ts'
 import { notFound } from './middleware/notFound.middleware.ts'
+import { fileURLToPath } from 'url'
 import authRoute from "./auth/auth.route.ts"
 import userRoute from "./modules/user/user.route.ts"
 import profileRoute from "./modules/profile/profile.route.ts"
@@ -18,12 +20,18 @@ const corsOption = {
     allowedHeaders: ["Content-Type", "Authorization"]
 }
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const project_root = path.resolve(__dirname, "..")
+
 app.use(express.json())
 app.use(cors(corsOption))
 
 app.get('/', (req, res) => {
     res.send('Hello, MindMesh!')
 })
+
+app.use("/upload", express.static(path.join(project_root, "public/images")))
 
 app.use("/auth", authRoute)
 app.use("/user", userRoute)
